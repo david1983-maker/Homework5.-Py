9e30575f264f4c19215eb7bf4bc7c6b08f091338
david1983-maker <davidkilidzan94127@gmail.com>
2024-06-18 19:50:39 +0300

    homework5.py

--- homework5.py
+++ homework5.py
@@ -8,7 +8,7 @@ imutable_var.append(20)
 print(imutable_var) 
 
 # создание изменяемых, стуктур данных:списки.
-imutable_var=["samurai", 15, True, 9.1]
-imutable_var.remove("samurai") 
-imutable_var.append("forest") 
-print(imutable_var)
+imutable_list=["samurai", 15, True, 9.1]
+imutable_list.remove("samurai") 
+imutable_list.append("forest") 
+print(imutable_list) 
